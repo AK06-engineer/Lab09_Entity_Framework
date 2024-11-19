874bcc7b63ffb8b2a1b3404b6423d5e7c41bcb54
PC832 <PC832@PC832>
2024-11-19 10:13:02 +0700

    Add project files.

---
+++ Lab09_Entity_Framework/MainForm.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Lab09_Entity_Framework
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.btnReloadCategory = new System.Windows.Forms.Button();
+            this.btnAddCategory = new System.Windows.Forms.Button();
+            this.tvwCategory = new System.Windows.Forms.TreeView();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnReloadFood = new System.Windows.Forms.Button();
+            this.btnAddFood = new System.Windows.Forms.Button();
+            this.lvwFood = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnReloadCategory
+            // 
+            this.btnReloadCategory.Location = new System.Drawing.Point(116, 33);
+            this.btnReloadCategory.Name = "btnReloadCategory";
+            this.btnReloadCategory.Size = new System.Drawing.Size(75, 23);
+            this.btnReloadCategory.TabIndex = 0;
+            this.btnReloadCategory.Text = "R";
+            this.toolTip1.SetToolTip(this.btnReloadCategory, "Tải lại danh mục");
+            this.btnReloadCategory.UseVisualStyleBackColor = true;
+            this.btnReloadCategory.Click += new System.EventHandler(this.btnReloadCategory_Click);
+            // 
+            // btnAddCategory
+            // 
+            this.btnAddCategory.Location = new System.Drawing.Point(207, 33);
+            this.btnAddCategory.Name = "btnAddCategory";
+            this.btnAddCategory.Size = new System.Drawing.Size(75, 23);
+            this.btnAddCategory.TabIndex = 1;
+            this.btnAddCategory.Text = "+";
+            this.toolTip1.SetToolTip(this.btnAddCategory, "Thêm danh mục mới");
+            this.btnAddCategory.UseVisualStyleBackColor = true;
+            // 
+            // tvwCategory
+            // 
+            this.tvwCategory.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.tvwCategory.Location = new System.Drawing.Point(11, 82);
+            this.tvwCategory.Name = "tvwCategory";
+            this.tvwCategory.Size = new System.Drawing.Size(271, 356);
+            this.tvwCategory.TabIndex = 2;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDelete.Location = new System.Drawing.Point(602, 33);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "-";
+            this.toolTip1.SetToolTip(this.btnDelete, "Xóa món ăn được chọn");
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnReloadFood
+            // 
+            this.btnReloadFood.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnReloadFood.Location = new System.Drawing.Point(511, 33);
+            this.btnReloadFood.Name = "btnReloadFood";
+            this.btnReloadFood.Size = new System.Drawing.Size(75, 23);
+            this.btnReloadFood.TabIndex = 3;
+            this.btnReloadFood.Text = "R";
+            this.toolTip1.SetToolTip(this.btnReloadFood, "Tải lại danh sách món ");
+            this.btnReloadFood.UseVisualStyleBackColor = true;
+            // 
+            // btnAddFood
+            // 
+            this.btnAddFood.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAddFood.Location = new System.Drawing.Point(695, 33);
+            this.btnAddFood.Name = "btnAddFood";
+            this.btnAddFood.Size = new System.Drawing.Size(75, 23);
+            this.btnAddFood.TabIndex = 5;
+            this.btnAddFood.Text = "+";
+            this.toolTip1.SetToolTip(this.btnAddFood, "Thêm món ăn mới");
+            this.btnAddFood.UseVisualStyleBackColor = true;
+            // 
+            // lvwFood
+            // 
+            this.lvwFood.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvwFood.FullRowSelect = true;
+            this.lvwFood.GridLines = true;
+            this.lvwFood.HideSelection = false;
+            this.lvwFood.Location = new System.Drawing.Point(314, 88);
+            this.lvwFood.MultiSelect = false;
+            this.lvwFood.Name = "lvwFood";
+            this.lvwFood.Size = new System.Drawing.Size(472, 349);
+            this.lvwFood.TabIndex = 6;
+            this.lvwFood.UseCompatibleStateImageBehavior = false;
+            this.lvwFood.View = System.Windows.Forms.View.Details;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(335, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Thực đơn";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Danh mục";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lvwFood);
+            this.Controls.Add(this.btnAddFood);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnReloadFood);
+            this.Controls.Add(this.tvwCategory);
+            this.Controls.Add(this.btnAddCategory);
+            this.Controls.Add(this.btnReloadCategory);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Quản lí nhà hàng";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button btnReloadCategory;
+        private System.Windows.Forms.Button btnAddCategory;
+        private System.Windows.Forms.TreeView tvwCategory;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnReloadFood;
+        private System.Windows.Forms.Button btnAddFood;
+        private System.Windows.Forms.ListView lvwFood;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Lab09_Entity_Framework/MainForm.cs
@@ -0,0 +1,48 @@
+﻿using Lab09_Entity_Framework.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab09_Entity_Framework
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnReloadCategory_Click(object sender, EventArgs e)
+        {
+
+        }
+        private List<Category> GetCategories()
+        {
+            var dbContext = new RestaurantContext();
+            return dbContext.Categories.OrderBy(x => x.Name).ToList();
+        }
+        private void ShowCategories()
+        {
+            tvwCategory.Nodes.Clear();
+
+            var cateMap = new Dictionary<CategoryType, string>();
+            {
+                [CategoryType.Food] = "Đồ ăn",
+                [CategoryType.Drink] = "Thức uống"
+            };
+
+            var rootNode = tvwCategory.Nodes
+        }
+    }
+}
